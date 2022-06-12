1dd3b711662c38c5cb52fde0cb303c796ca84aa2
Marquis <smcneal89@gmail.com>
2022-06-12 15:43:31 -0400

    added server code, index file and server.js

---
+++ routes/api.js
@@ -0,0 +1,5 @@
+const route = require("express").Router();
+
+route.get("/notes", (req, res) => {});
+
+module.exports = route;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const api = require("./routes/api");
+const html = require("./routes/html");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use("/api", api);
+app.use("/", html);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
